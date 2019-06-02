88a9ce2035923ff5ff972a544f4281a7c74fbf0e
Ratiekae <ratiekae17@gmail.com>
2019-06-02 07:29:28 -0500

    funeral code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CharacterCreator
 
 Another learning project
+
+Starting small here, just to test a few small features, before finding the time to move this to a bigger and more complex tech stack
--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const startScreen = document.getElementById("startScreen");
 const characterScreen = document.getElementById("charScreen");
 
+const nameChar = document.getElementById("name-char");
+const raceChar = document.getElementById("race-char");
+const classChar = document.getElementById("class-char");
+const levelChar = document.getElementById("level-char");
+const hometownChar = document.getElementById("hometown-char");
+const lawChar = document.getElementById("lawfulness-char");
+const goodChar = document.getElementById("goodness-char");
+
+
+
 var playerChar = {
   name: "",
   race: "",
@@ -12,24 +22,38 @@ var playerChar = {
 };
 
 function characterCreate() {
-  var name_id = document.getElementById("name").value;
-  var race_id = document.getElementById("races").value;
-  var class_id = document.getElementById("classes").value;
-  var level_id = document.getElementById("level").value;
-  var startArea_id = document.getElementById("start-area").value;
-  var lawAlign_id = document.getElementById("law-alignment").value;
-  var goodAlign_id = document.getElementById("good-alignment").value;
+  
+  playerChar.name = document.getElementById("name").value;
+  playerChar.race = document.getElementById("races").value;
+  playerChar.class = document.getElementById("classes").value;
+  playerChar.level = document.getElementById("level").value;
+  playerChar.hometown = document.getElementById("start-area").value;
+  playerChar.lawfulness = document.getElementById("law-alignment").value;
+  playerChar.goodness = document.getElementById("good-alignment").value;
 
+/*
   playerChar.name = name_id;
   playerChar.race = race_id;
   playerChar.class = class_id;
   playerChar.level = level_id;
   playerChar.hometown = startArea_id;
   playerChar.lawfulness = lawAlign_id;
-  playerChar.goodness = goodAlign_id;
+  playerChar.goodness = goodAlign_id;*/
   console.log(playerChar);
   characterScreen.style.display = "block";
   startScreen.style.display = "none";
+  generateCharSheet();
+}
+
+
+function generateCharSheet (){
+  nameChar.innerHTML = playerChar.name;
+  raceChar.innerHTML = playerChar.race;
+  classChar.innerHTML = playerChar.class;
+  levelChar.innerHTML = playerChar.level;
+  hometownChar.innerHTML = playerChar.hometown;
+  lawChar.innerHTML = playerChar.lawfulness;
+  goodChar.innerHTML = playerChar.goodness;
 }
 
 function main() {
